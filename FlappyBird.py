d59647e36b5706d4026525801c46ef5a74bc719b
Layr Bastos <102252629+LayrBastos@users.noreply.github.com>
2025-10-14 19:05:49 -0300

    game.py

--- FlappyBird.py
+++ FlappyBird.py
@@ -1,18 +1,12 @@
 import pygame
 import os
 import random
+from game import Base, Bird, Pipe
 
 screen_width = 500
 screen_height = 800
 
-pipe_img = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'pipe.png')))
-floor_img = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'base.png')))
-bg_img = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bg.png')))
-bird_imgs = [
-    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird1.png'))),
-    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird2.png'))),
-    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird3.png'))),
-]
+
 
 pygame.font.init()
 SCORE_FONT = (pygame.font.SysFont('arial', 50))
---
+++ game.py
@@ -0,0 +1,44 @@
+import pygame
+
+PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'pipe.png')))
+BASE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'base.png')))
+BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bg.png')))
+BIRD_IMGS = [
+    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird1.png'))),
+    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird2.png'))),
+    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird3.png'))),
+]
+
+class Bird:
+    IMGS = BIRD_IMGS
+    MAX_ROTATION = 25
+    ROTATION_SPEED = 20
+    ANIMATION_TIME = 5
+
+    def __init__(self, x_position, y_position):
+        self.x = x_position
+        self.y = y_position
+        self.angle = 0
+        self.speed = 0
+        self.height = self.y
+        self.movement_time = 0
+        self.img_count = 0
+        self.img = IMGS[0]
+
+    def jump(self):
+        self.speed = -10.5
+        self.time = 0
+        self.height = self.y
+
+    def move(self):
+        
+
+
+
+class Pipe:
+    pass
+
+
+class Base:
+    pass
+
